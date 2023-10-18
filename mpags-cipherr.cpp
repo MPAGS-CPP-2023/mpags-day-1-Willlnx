ca5747fa424069673aafde9252c6295010d45249
willlnx <williambishop56@gmail.com>
2023-10-18 15:15:55 +0000

    Completed homework for cpp MPAG week 1, may have a missing part due to missing the final 2 hours of the lecture.

---
+++ mpags-cipherr.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    char in_char{'x'};
+    std::string out_str{""};
+
+    white (std::cin >> in_char)
+    {
+        std::cout << in_char << std::endl;
+    }
+}
